fec245082bc8aeb29274e075f06ab1acb6839d74
hippy <hippy2981@gmail.com>
2019-11-30 13:22:55 -0800

    build: Target new frameworks
    netstandard2.1, netcoreapp3.0

--- ci.sh
+++ ci.sh
@@ -2,10 +2,10 @@
 
 dotnet restore
 dotnet clean -c Release
-# netcoreapp2.0 only as test csproj also targets others
+# netcoreapp3.0 only as test csproj also targets others
 dotnet build Trie.sln \
 	-c Release \
-	-f netcoreapp2.0
+	-f netcoreapp3.0
 dotnet test tests/rm.TrieTest/rm.TrieTest.csproj \
 	-c Release --no-build --no-restore \
-	-f netcoreapp2.0
+	-f netcoreapp3.0
