23e009045aba1f95fda48966458baf062a4aa17a
hippy <hippy2981@gmail.com>
2019-12-04 00:22:34 -0800

    build: Target new frameworks
    netcoreapp3.1

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ dotnet clean -c Release
 # netcoreapp3.0 only as test csproj also targets others
 dotnet build Trie.sln \
 	-c Release \
-	-f netcoreapp3.0
+	-f netcoreapp3.1
 dotnet test tests/rm.TrieTest/rm.TrieTest.csproj \
 	-c Release --no-build --no-restore \
-	-f netcoreapp3.0
+	-f netcoreapp3.1
